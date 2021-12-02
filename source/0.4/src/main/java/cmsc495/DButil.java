ef81401407e1111ed905d341a499862a75f08bb4
joshroche1 <joshua.a.roche@gmail.com>
2021-12-02 12:33:24 +0100

    Update DButil.java

--- source/0.4/src/main/java/cmsc495/DButil.java
+++ source/0.4/src/main/java/cmsc495/DButil.java
@@ -209,31 +209,31 @@ public class DButil {
 	// Get Medication records associated with Patient username and Provider username
   public String getMedications(String pt, String pr) {
     rs = null;
+		String temp = "<table class=&quot;w3-table-all w3-responsive&quot;><tr><th>Name</th><th>Dosage</th><th>Unit</th><th>Issued</th><th>Expires</th></tr>";
     try {
       c = this.connect();
       PreparedStatement stmt = c.prepareStatement("SELECT name, dosage, doseUnit, issueDate, expDate FROM Medication WHERE patient=? AND provider=?",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
       stmt.setString(1, pt);
 			stmt.setString(1, pr);
       rs = stmt.executeQuery();
+			while (rs.next()) {
+				temp += "<tr><td>";
+				temp += "" + rs.getString("name");
+				temp += "</td><td>";
+				temp += "" + rs.getDouble("dosage");
+				temp += "</td><td>";
+				temp += "" + rs.getString("doseUnit");
+				temp += "</td><td>";
+				temp += "" + rs.getString("issueDate");
+				temp += "</td><td>";
+				temp += "" + rs.getString("expDate");
+				temp += "</td></tr>";
+			}
+		temp += "</table>";
     } catch (SQLException ex) {
       FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
                    FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
     }
-		String temp = "<table class=&quot;w3-table-all w3-responsive&quot;><tr><th>Name</th><th>Dosage</th><th>Unit</th><th>Issued</th><th>Expires</th></tr>";
-		while (rs.next()) {
-			temp += "<tr><td>";
-			temp += "" + rs.getString("name");
-			temp += "</td><td>";
-			temp += "" + rs.getDouble("dosage");
-			temp += "</td><td>";
-			temp += "" + rs.getString("doseUnit");
-			temp += "</td><td>";
-			temp += "" + rs.getString("issueDate");
-			temp += "</td><td>";
-			temp += "" + rs.getString("expDate");
-			temp += "</td></tr>";
-		}
-		temp += "</table>";
     return temp;
   }
   // Get Patient record by username
