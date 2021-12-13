76082e009cbcb677c9f02b498c4d2aeea4f8ca28
Fawziyah Alebiosu <fawziyah.alebiosu@gmail.com>
2021-12-13 17:37:47 -0500

    Create patientTests.java

---
+++ source/0.4f/src/main/tests/patientTests.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class patientTests {
+    
+    @Test 
+     registerPatientHappyPath() {
+
+        fail();
+    }
+    registerPatientSadPath(){
+       fail();
+
+    }
+    void authenticateHappyPath(){
+        fail();
+    }
+    void authenticateSadPath(){
+        fail();
+
+    }
+    void updateProfileHappyPath(){
+        fail();
+    }
+
+    void updateProfileSadPath(){
+        fail();
+    }
+   
+    void updatePasswordSadPath(){
+        fail();
+
+    }
+
+    void updatePasswordHappyPath(){
+        fail();
+
+
+    }
+
+    void getMedListSadPath(){
+        fail();
+
+    }
+
+    void getMedListHappyPath(){
+        fail();
+    }
+}
