1111c6f003fdb388a3d1a72c9035baa9bf6a16c2
Fawziyah Alebiosu <fawziyah.alebiosu@gmail.com>
2021-12-13 17:38:31 -0500

    Create providerTests.java

---
+++ source/0.4f/src/main/tests/providerTests.java
@@ -0,0 +1,77 @@
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class providerTests {
+    
+    @Test 
+     void registerProviderHappyPath() {
+
+        fail();
+    }
+    void registerProviderSadPath(){
+        fail();
+
+    }
+    void authenticateProviderHappyPath(){
+        fail();
+
+    }
+    void authenticateProviderSadPath(){
+        fail();
+
+    }
+    void updateProfileProviderHappyPath(){
+        fail();
+    }
+
+    void updateProfileProviderSadPath(){
+        fail();
+
+    }
+   
+    void updatePasswordProviderSadPath(){
+        fail();
+
+    }
+
+    void updatePasswordProviderHappyPath(){
+        fail();
+
+
+    }
+
+    void getPatientListSadPath(){
+        fail();
+
+    }
+
+    void getPatientListHappyPath(){
+        fail();
+    }
+    
+    void updatePatientHappyPath(){
+        fail();
+    }
+
+    void updatePatientSadPath(){
+        fail();
+    }
+
+    void addMedicationHappyPath(){
+        fail();
+    }
+    void addMedicationSadPath(){
+        fail();
+    }
+    void delMedicationHappyPath(){
+        fail();
+
+    }
+    void delMedicationSadPath(){
+        fail();
+
+    }
+
+}
